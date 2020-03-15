60886f683bb13b1bb06ff7c325dde74dfb812a65
天明 <1154706249@qq.com>
2020-03-15 11:08:02 +0800

    注册使用vant

--- src/main.js
+++ src/main.js
@@ -2,9 +2,10 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-
+import Vant from 'vant'
+import 'vant/lib/index.css'
 Vue.config.productionTip = false
-
+Vue.use(Vant)// 注册vant 完整导入 优化时可按需导入{button} Vue.use(button)
 new Vue({
   router,
   store,
