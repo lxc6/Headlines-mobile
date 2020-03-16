e80ebb6a225aa8999002c1698078a4ceb6d90dc8
天明 <1154706249@qq.com>
2020-03-16 11:24:18 +0800

    vuex状态管理token

--- src/store/index.js
+++ src/store/index.js
@@ -1,12 +1,21 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
-
+import * as auth from '@/utils/auth'
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    user: auth.getUser()
   },
   mutations: {
+    updataUser (state, payload) {
+      state.user = payload.user// 更新数据
+      auth.setUser(payload.user)// 数据同步到本地存储
+    },
+    clearUser (state) {
+      state.user = {} // 清空数据
+      auth.delUser()// 清除缓存数据
+    }
   },
   actions: {
   },
