b334cdb2e0186922d25413d1746bcb84ec23b65e
天明 <1154706249@qq.com>
2020-03-18 21:35:29 +0800

    主页简单构建结构

---
+++ src/views/home/components/article-list.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="scroll-wrapper">
+      <van-list v-model="upLoading" :finished="finished" @load="onLoad"></van-list>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      upLoading: false, // 表示是否开启了上拉加载 默认值false
+      finished: false // 表示 是否已经完成所有数据的加载
+    }
+  },
+  methods: {
+    onLoad () {
+      console.log('开始加载数据')
+      setTimeout(() => {
+        this.finished = true
+        console.log('加载数据完成')
+      }, 1000)
+    }
+  }
+}
+</script>
+
+<style>
+</style>
--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,19 +1,82 @@
 <template>
-  <div>
-    <div class="split">home</div>
-    <van-skeleton title avatar :row="3" />
-    <div class="split"></div>
-    <van-skeleton title avatar :row="3" />
-    <div class="split"></div>
-    <van-skeleton title avatar :row="3" />
-    <div class="split"></div>
-    <van-skeleton title avatar :row="3" />
+  <div class="container">
+    <!-- tab标签页 -->
+    <van-tabs>
+      <van-tab :title="`标签${item}`" v-for="item in 10" :key="item">
+        <ArticleList></ArticleList>
+      </van-tab>
+    </van-tabs>
+    <!-- 综合 -->
+    <span class="bar_btn">
+      <van-icon name="wap-nav" />
+    </span>
   </div>
 </template>
 
 <script>
-export default {}
+import ArticleList from './components/article-list'
+export default {
+  components: {
+    ArticleList
+  }
+}
 </script>
 
-<style>
+<style lang='less' scoped>
+.van-tabs {
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  /deep/ .van-tabs__wrap {
+    height: 36px;
+    padding-right: 36px;
+    .van-tab {
+      line-height: 36px;
+      background: rgba(231, 11, 11, 0.1)
+    }
+    .van-tabs__line {
+      background-color: #3296fa;
+      height: 2px;
+    }
+  }
+  /deep/ .van-tabs__content {
+    flex: 1;
+    overflow: hidden;
+  }
+  /deep/ .van-tab__pane {
+    height: 100%;
+    .scroll-wrapper {
+      height: 100%;
+      overflow-y: auto;
+    }
+  }
+}
+.bar_btn {
+  width: 36px;
+  height: 35px;
+  position: absolute;
+  top: 0;
+  right: 0;
+  &::before {
+    content: "";
+    width: 100%;
+    height: 100%;
+    position: absolute;
+    z-index: 999;
+    box-shadow: 0 0 10px #999;
+    transform: scale(1, 0.6);
+  }
+  .van-icon-wap-nav {
+    width: 100%;
+    height: 100%;
+    background: #fff;
+    text-align: center;
+    line-height: 35px;
+    position: relative;
+    z-index: 1000;
+    &::before {
+      font-size: 20px;
+    }
+  }
+}
 </style>
